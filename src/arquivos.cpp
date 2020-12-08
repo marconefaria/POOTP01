e52f094af7617f741e7c846260b6bf3618e72504
Marcone Faria <marconefaria@cpejr.com.br>
2020-12-08 17:54:28 -0300

    Início código compressor.cpp

--- src/arquivos.cpp
+++ src/arquivos.cpp
@@ -2,4 +2,3 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
-
--- src/compressor.cpp
+++ src/compressor.cpp
@@ -2,3 +2,40 @@
 #include <vector>
 #include <cstdio>
 using namespace std;
+
+int main(){
+    int numero;
+    vector<int> numeros;
+    vector<int> repetidos;
+
+    cin >> numero;
+
+    if(numero >= 0){
+        while (numero >= 0){
+            numeros.push_back(numero);
+            cin >> numero;
+        }
+
+        for(int i = 0; i < numeros.size(); i++){
+            cout << numeros[i] << " ";
+        }
+
+        int cont = 1;
+        for(int i = 0; i < numeros.size(); i++){
+            if(numeros[i] == numeros[i+1]){
+                cont++;
+            }
+            repetidos.push_back(cont);
+            repetidos.push_back(numeros[i]);
+        }
+
+        cout << "\n";
+
+        for(int i = 0; i < repetidos.size(); i++){
+            cout << repetidos[i] << " ";
+        }
+
+    }else{
+        return 1;
+    }    
+}
