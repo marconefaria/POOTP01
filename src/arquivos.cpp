06d9fc166f561565975481e7eefdba6e6942bc1c
Marcone Faria <marconefaria@cpejr.com.br>
2020-12-08 16:50:46 -0300

    Configurações Iniciais

---
+++ src/arquivos.cpp
@@ -0,0 +1,5 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
---
+++ src/matriz.hpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+typedef struct{
+    double** m;
+    int nLinhas;
+    int nColunas;
+}Matriz;
+
+void inicializaMatriz(Matriz &X, int ls, int cs, const double elem){
+
+}
+
+void apagaMatriz(Matriz &X){
+
+}
+
+Matriz transposta(Matriz &X){
+
+}
+
+Matriz multiplica_por_cte(Matriz &X, double k){
+
+}
+
+void imprimeMatriz(Matriz &X){
+    
+}
