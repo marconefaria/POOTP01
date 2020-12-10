48421ca23fd3730ac54fe9d58b316c9bb10b6f7d
Marcone Faria <marconefaria@cpejr.com.br>
2020-12-10 15:47:07 -0300

    arquivos.cpp pronto

--- src/arquivos.cpp
+++ src/arquivos.cpp
@@ -2,3 +2,29 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
+
+int main()
+{
+  FILE *arq;
+  char *result;
+  int cont=0;
+  char c;
+  // Abre um arquivo TEXTO para LEITURA
+  arq = fopen("file.txt", "a+");
+  if (arq == NULL)  // Se houve erro na abertura
+  {
+     printf("Problemas na abertura do arquivo\n");
+     return 1;
+  }
+
+  while (!feof(arq))
+  {
+	// Lê uma linha (inclusive com o '\n')
+	if((c = fgetc(arq)) !=EOF)
+        if(c == ' ')
+            cont++;
+  }
+  fclose(arq);
+  cout << cont;
+  return 0;
+}
--- src/matriz.hpp
+++ src/matriz.hpp
@@ -8,7 +8,14 @@ typedef struct{
 }Matriz;
 
 void inicializaMatriz(Matriz &X, int ls, int cs, const double elem){
-
+    X.nLinhas = ls;
+    X.nColunas = cs;
+    for(int i=0; i < ls; i++){
+        for(int j=0; j < cs; j++){
+            cin
+            X.m[i][j] = elem;
+        }
+    }
 }
 
 void apagaMatriz(Matriz &X){
@@ -16,13 +23,18 @@ void apagaMatriz(Matriz &X){
 }
 
 Matriz transposta(Matriz &X){
-
+    return X;
 }
 
 Matriz multiplica_por_cte(Matriz &X, double k){
-
+    return X;
 }
 
 void imprimeMatriz(Matriz &X){
-    
+    for(int i=0; i < X.nLinhas; i++){
+        for(int j=0; j < X.nColunas; j++){
+            cout << X.m[i][j] << "\t";
+        }
+        cout << "\n";
+    }
 }
